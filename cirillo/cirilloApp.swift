18a63bd575e446df99f5b797bdedc1006bef1db0
Leonardo <leonardoorosaa16@gmail.com>
2023-06-13 20:35:32 -0300

    Initial Commit

---
+++ cirillo/cirilloApp.swift
@@ -0,0 +1,17 @@
+//
+//  cirilloApp.swift
+//  cirillo
+//
+//  Created by Leonardo Rosa on 13/06/23.
+//
+
+import SwiftUI
+
+@main
+struct cirilloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
